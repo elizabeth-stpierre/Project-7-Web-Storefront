c5d524ade3bb86ca4c7a0000790314259ead48bf
darksabo <edwin.pena489@west-mec.org>
2017-01-13 18:06:59 -0700

    updated
    javascript for application

---
+++ js/apply.js
@@ -0,0 +1,36 @@
+/**
+ * Created by edwin.pena489 on 1/13/17.
+ */
+alert("Please, Fill Out All Information Provided\n Thank You For Choosing Medical Donation");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
